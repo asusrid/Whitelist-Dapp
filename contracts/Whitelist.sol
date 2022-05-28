eb5e3c15b4f62144bf0205fcb227d4495ff88950
asusrid <alexsurid@gmail.com>
2022-05-28 13:06:35 +0200

    contract Whitelist

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -4,9 +4,22 @@ pragma solidity ^0.8.0;
 contract Whitelist {
 
     uint8 public maxAllowedUsers;
+    //uint8 public numAllowedUsers;
+    uint8 public numAddressesWhitelisted;
+
+    mapping(address => bool) public whitelistedAddresses;
 
     constructor(uint8 _maxAllowedUsers) {
         maxAllowedUsers = _maxAllowedUsers;
+    }
+
+    function addAdressToWhitelist() public {
+        require(!whitelistedAddresses[msg.sender], "Sender is already registered!");
+        require(numAddressesWhitelisted < maxAllowedUsers, "You cannot create more users!");
+
+        // register user
+        whitelistedAddresses[msg.sender] = true;
+        numAddressesWhitelisted += 1;
     }
 
 }
